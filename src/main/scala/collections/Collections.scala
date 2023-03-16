22b86938026864d4c0d5780aedf13b78cc06357b
Nikita Kalinskiy <32264994+nkalinskiy@users.noreply.github.com>
2023-03-16 11:29:26 +0400

    Update Collections.scala

--- src/main/scala/collections/Collections.scala
+++ src/main/scala/collections/Collections.scala
@@ -2,39 +2,12 @@ package collections
 
 object Collections {
 
-  // Return a tail of a list
-  def tail[A](l: List[A]): List[A] = ???
-
-  // Set a head of a list to a new one
-  def setHead[A](l: List[A], h: A): List[A] = ???
-
-  // Drop n elements of the list. If there are
-  // less than n elements - just return None
-  def drop[A](l: List[A], n: Int): List[A] = ???
-
-  // Drop elements while they satisfy the condition
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
-
-  // Return all the elements of the list except the last one
-  // If None - return None
-  def init[A](l: List[A]): List[A] = ???
-
-  // Return the length of the list
-  def length[A](l: List[A]): Int = ???
-
-  // Traverse the list, accumulating value z, using some function f
-  // Try making it tail recursive
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
-
-  // Map elements of type A to elements of type B
-  def map[A, B](l: List[A])(f: A => B): List[B] = ???
-
   /*
     In a sorted list find all pairs of two neighbor numbers which have a gap between them
     None for Seq(1, 2, 3, 4)
     Some(Seq((2, 8))) for Seq(1, 2, 8)
     Some(Seq((3, 5), (5, 7))) for Seq(3, 5, 7)
-   */
+  */
   def findGaps(l: Seq[Int]): Option[Seq[(Int, Int)]] = {
     ???
   }
@@ -53,7 +26,7 @@ object Collections {
 
   def minRecursion(map: Map[String, Int]): Option[(String, Int)] = ???
 
-  // Implement scanLeft (not using scans ofc)
+  // Implement scanLeft - running total, applying [f] to elements of [list] (not using scans ofc)
   def scanLeft[T](zero: T)(list: Seq[T])(f: (T, T) => T): Seq[T] = {
     ???
   }
